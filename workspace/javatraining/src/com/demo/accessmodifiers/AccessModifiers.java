6573b5a3b199836dcf93b4b727a05ad814effea3
Java Learner <fbjavalearners@gmail.com>
2016-12-21 13:47:10 +0530

    Added

--- workspace/javatraining/src/com/demo/accessmodifiers/AccessModifiers.java
+++ workspace/javatraining/src/com/demo/accessmodifiers/AccessModifiers.java
@@ -1,5 +1,16 @@
 package com.demo.accessmodifiers;
-
+/*
+ * Class can be added with 2 modifiers
+ *   a)(default)if you don't give any thing it is default access and class is accessible
+ *   only with in package
+ *   
+ *   b)(public) It can be accessed from anywhere from any package. Outside the package
+ *   you need to add import statement first and then use it
+ * 
+ * 
+ * 
+ * 
+ */
 public class AccessModifiers {
 
 	public static void main(String args[]){
