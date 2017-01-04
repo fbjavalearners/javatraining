ec7ffbe2eb34d99ed434415d21acbb2c16d17a4a
Java Learner <fbjavalearners@gmail.com>
2017-01-04 10:47:28 +0530

    Jdbc examples

--- workspace/javatraining/src/com/demo/io/BufferedReaderExample.java
+++ workspace/javatraining/src/com/demo/io/BufferedReaderExample.java
@@ -3,11 +3,16 @@ package com.demo.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
 
 public class BufferedReaderExample {
-
 	
 	public static void main(String[] args) throws IOException {
+		
+	
+		
+		
 		
 		 String  thisLine = null;
 			      try{
@@ -21,7 +26,21 @@ public class BufferedReaderExample {
 			      }
 		
 		
-		
+			      try{
+				         // open input stream test.txt for reading purpose.
+				         LineNumberReader lr = new LineNumberReader(new FileReader("R:\\all\\files\\one.txt"));
+				         
+				         while ((thisLine = lr.readLine()) != null) {
+				        	 
+				        	 System.out.println("Line is :: "+thisLine+" Line no: "+lr.getLineNumber());
+				        	  
+				        	 
+				         }
+				      }catch(Exception e){
+				         e.printStackTrace();
+				      }
+			
+			
 		
 		
 		
