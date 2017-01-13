0fd0b2b9ffa165ba46710fad23622d5277de2042
Java Learner <fbjavalearners@gmail.com>
2017-01-13 08:51:25 +0530

    Modified student class

--- workspace/javatraining/src/com/demo/oracleex/ResultSetExample.java
+++ workspace/javatraining/src/com/demo/oracleex/ResultSetExample.java
@@ -12,57 +12,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ResultSetExample  extends Thread{
+public class ResultSetExample  {
  
-	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
-		try {
-		 
-			myTest();
-		  
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
-	public static void myTest() throws SQLException {
+	public static void main(String args[]) throws SQLException {
+		//Fetching employees data
+		
 		Connection con = ConnectionUtil.getConnection();
-		String sqlQuery = "SELECT * FROM employees where employee_id=1112 ";
+		String sqlQuery = "SELECT * FROM employees  ";
 		Statement st = null;
 		ResultSet rs = null;
 		
-		st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
-		
+		st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+		//st = con.createStatement();
 		
 		rs = st.executeQuery(sqlQuery);
 		
-	  
-		  try {
-			Thread.sleep(20000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			if(rs.next())
+	  /*
+			while(rs.next())
 		  System.out.println(rs.getInt(1)+ rs.getString("first_name"));
-			
+			*/
 	
 		
 		//System.out.println("Iterating backwards ");
-		/*while(rs.previous()){
+		/*if(rs.previous()){
 			System.out.println(rs.getInt(1));
 		}*/
 		
-	    
+	    rs.absolute(10000);
+	    System.out.println(rs.getInt(1)+ rs.getString("first_name"));
 	   
 		
-	}
-	public static void main(String[] args) {
-		new ResultSetExample().start();
 	}
 	
 	
